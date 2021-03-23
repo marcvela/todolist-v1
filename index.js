e497b15f3191bf2f2c317cce365fb650d4bcd4a4
marcvela <marcvela@hawaii.edu>
2021-03-23 21:26:51 +0000

    Added /fun and /weekend URL routes. Have yet to add buttons.

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const app = express();
 // set an array for the default items in the list
 let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
 // set an empty array for new work items
-let workItems = ["Show Up", "Get Settled"];
+let workItems = ["Show Up", "Get Settled", "Check Email", "Begin Work", "Talk to Corporate", "Approve Memos"];
+// set an empty array for fun items
+let funItems = ["Drink Liquor", "Watch Other People Drink Liquor", "Play Games", "Watch Other People Play Games"]
+// set an empty array for weekend items
+let wkndItems = ["Go Beach", "Sleep In Late", "Skip Showering", "Make Foolish Purchases"]
 
 // set EJS as the viewing engine to display html
 app.set('view engine', 'ejs');
 
 // use body parser to parse html file
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // use Express to serve or display static files such as images, CSS, JS files etc.
 app.use(express.static("public"));
 
@@ -25,35 +29,48 @@ app.use(express.static("public"));
 // default html file in web server
 app.get("/", function(req, res) {
 
-    //get the system date from the getDate function exported by the date.js file
-    let day = date.getDate();
-    
-    // use EJS render to display the day and the To Do List
-    res.render("list", {listTitle: day, newListItems: items});
-    
+  //get the system date from the getDate function exported by the date.js file
+  let day = date.getDate();
+
+  // use EJS render to display the day and the To Do List
+  res.render("list", { listTitle: day, newListItems: items });
+
 });
 
 // display default to do list on the default root folder
 app.post("/", function(req, res) {
-    
-    
-    // code allows items to be added to the regular list and work list
-    let item = req.body.newItem;
-    
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
+
+
+  // code allows items to be added to the regular list and work list
+  let item = req.body.newItem;
+
+  if (req.body.list === "Weekend") {
+    wkndItems.push(item);
+    res.redirect("/weekend");
+  }
+  if (req.body.list === "Fun") {
+    funItems.push(item);
+    res.redirect("/fun");
+  }
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 // display default to do list on the localhost:3000/work route!
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work To Do List", newListItems: workItems })
 });
-
+app.get("/fun", function(req, res) {
+  res.render("list", { listTitle: "Fun To Do List", newListItems: funItems })
+});
+app.get("/weekend", function(req, res) {
+  res.render("list", { listTitle: "Weekend To Do List", newListItems: wkndItems })
+});
 app.listen(3000, function() {
-console.log ("Server is running on port 3000")
+  console.log("Server is running on port 3000")
 });
