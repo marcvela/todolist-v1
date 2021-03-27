094b58560ef9180d627e9b70508b8504ef41dd66
marcvela <marcvela@hawaii.edu>
2021-03-27 06:41:40 +0000

    change if statements to else if

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
 // set an empty array for new work items
 let workItems = ["Show Up", "Get Settled", "Check Email", "Begin Work", "Talk to Corporate", "Approve Memos"];
 // set an empty array for fun items
-let funItems = ["Drink Liquor", "Watch Other People Drink Liquor", "Play Games", "Watch Other People Play Games"]
+let funItems = ["Drink Liquor", "Watch Other People Drink Liquor", "Play Games", "Watch Other People Play Games"];
 // set an empty array for weekend items
-let wkndItems = ["Go Beach", "Sleep In Late", "Skip Showering", "Make Foolish Purchases"]
+let weekendItems = ["Go Beach", "Sleep In Late", "Skip Showering", "Make Foolish Purchases"];
 
 // set EJS as the viewing engine to display html
 app.set('view engine', 'ejs');
 
 // use body parser to parse html file
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // use Express to serve or display static files such as images, CSS, JS files etc.
 app.use(express.static("public"));
-
 
 // default html file in web server
 app.get("/", function(req, res) {
@@ -40,22 +40,22 @@ app.get("/", function(req, res) {
 // display default to do list on the default root folder
 app.post("/", function(req, res) {
 
-
-  // code allows items to be added to the regular list and work list
+// code allows items to be added to the regular list and work list
   let item = req.body.newItem;
 
   if (req.body.list === "Weekend") {
-    wkndItems.push(item);
+    weekendItems.push(item);
     res.redirect("/weekend");
   }
-  if (req.body.list === "Fun") {
+  else if (req.body.list === "Fun") {
     funItems.push(item);
     res.redirect("/fun");
   }
-  if (req.body.list === "Work") {
+  else if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-  } else {
+  }
+  else {
     items.push(item);
     res.redirect("/");
   }
@@ -69,7 +69,7 @@ app.get("/fun", function(req, res) {
   res.render("list", { listTitle: "Fun To Do List", newListItems: funItems })
 });
 app.get("/weekend", function(req, res) {
-  res.render("list", { listTitle: "Weekend To Do List", newListItems: wkndItems })
+  res.render("list", { listTitle: "Weekend To Do List", newListItems: weekendItems })
 });
 app.listen(3000, function() {
   console.log("Server is running on port 3000")
